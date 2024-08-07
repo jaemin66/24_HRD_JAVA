fc5050b93c25ee7b3dbd53fa0d2e234d065d1c31
jaemin66 <jaemin20636@gmail.com>
2024-08-07 09:53:54 +0900

    Add files via upload

---
+++ day02.java
@@ -0,0 +1,33 @@
+package day01;
+import java.util.Scanner;
+public class day02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int jumsu = 90;
+		System.out.print("점수를 입력하세요:");
+		jumsu = sc.nextInt();
+		String hakjum = "";
+		
+		if(jumsu >= 95) {
+			hakjum = "A+";
+		}else if(jumsu >= 85) {
+			hakjum = "A";
+		}else if(jumsu >= 70) {
+			hakjum = "B+";
+		}else if(jumsu >= 60) {
+			hakjum = "B";
+		}else {
+			hakjum = "C";
+		}
+		
+		System.out.println("당신의 학점은: " + hakjum + "입니다");
+		
+
+	}
+
+}
